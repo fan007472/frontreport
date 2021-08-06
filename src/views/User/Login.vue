cd5462035dd596691c44302023db22555abdfb26
LF <lingjie.fan@zurich.com.cn>
2021-08-06 16:36:29 +0800

    003

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -30,7 +30,7 @@ import { validate, resetFields } from '@/utils/formExtend'
 const formRender = () => {
     const form = reactive({
         name: 'admin',
-        pwd: 'admin'
+        pwd: ''
     })
     const ruleForm = ref(null)
     const enterSubmit = (e: KeyboardEvent) => {
