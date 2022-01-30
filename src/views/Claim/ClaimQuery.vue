c320e5b7e85cedb7215c7e7c1d868e08e9bf1994
LF <fan007472@gmail.com>
2022-01-30 22:24:52 +0800

    1

--- src/views/Claim/ClaimQuery.vue
+++ src/views/Claim/ClaimQuery.vue
@@ -158,14 +158,14 @@
                 </el-form-item>
             </el-form>
         </el-card>
+        <div>
+            <el-table :data='tableData' stripe style='width: 100%'>
+                <el-table-column prop='date' label='Date' width='180' />
+                <el-table-column prop='name' label='Name' width='180' />
+                <el-table-column prop='address' label='Address' />
+            </el-table>
+        </div>
     </div>
-    <!-- <div>
-        <el-table :data='tableData' stripe style='width: 100%'>
-            <el-table-column prop='date' label='Date' width='180' />
-            <el-table-column prop='name' label='Name' width='180' />
-            <el-table-column prop='address' label='Address' />
-        </el-table>
-    </div> -->
 </template>
 
 <script lang="ts">
