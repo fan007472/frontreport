efc6efbd14977675365cc3b32e52c47b87254604
lingjie.fan <lingjie.fan@zurich.com.cn>
2022-03-10 16:14:49 +0800

    login

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class='layout-login' @keyup='enterSubmit'>
-        <el-form
-            ref='ruleForm'
-            label-position='right'
-            label-width='80px'
-            :model='form'
-            :rules='rules'
-        >
-            <el-form-item label='用户名' prop='name'>
-                <el-input v-model='form.name' />
-            </el-form-item>
-            <el-form-item label='密码' prop='pwd'>
-                <el-input v-model='form.pwd' type='password' autocomplete='off' />
-            </el-form-item>
-            <el-form-item>
-                <el-button type='primary' @click='onSubmit'> 登录 </el-button>
-                <el-button @click='resetFields(ruleForm)'> 重置 </el-button>
-            </el-form-item>
-        </el-form>
+    <div :style='{height:&apos;100%&apos;, backgroundImage:&apos;url(&apos;+background+&apos;)&apos;}'>
+        &nbsp;
+        <div class='layout-login' @keyup='enterSubmit'>
+            <el-form
+                ref='ruleForm'
+                label-position='right'
+                label-width='80px'
+                :model='form'
+                :rules='rules'
+            >
+                <el-form-item label='用户名' prop='name' class='change-label-class'>
+                    <el-input v-model='form.name' placeholder='请输入您的Windows开机账号' />
+                </el-form-item>
+                <el-form-item label='密码' prop='pwd' class='change-label-class'>
+                    <el-input v-model='form.pwd' type='password' autocomplete='off' placeholder='同您的Windows开机密码' show-password/>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type='primary' @click='onSubmit'> 登录 </el-button>
+                    <el-button @click='resetFields(ruleForm)'> 重置 </el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
@@ -26,11 +29,12 @@ import { defineComponent, reactive, ref } from 'vue'
 import { store } from '@/store/index'
 import { ElNotification } from 'element-plus'
 import { validate, resetFields } from '@/utils/formExtend'
+import background from '@/assets/img/background2.jpg'
 
 const formRender = () => {
     const form = reactive({
-        name: 'admin',
-        pwd: 'admin'
+        name: '',
+        pwd: ''
     })
     const ruleForm = ref(null)
     const enterSubmit = (e: KeyboardEvent) => {
@@ -86,7 +90,8 @@ const formRender = () => {
         enterSubmit,
         rules,
         resetFields,
-        ruleForm
+        ruleForm,
+        background
     }
 }
 export default defineComponent({
@@ -101,10 +106,16 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
+<style>
 .layout-login {
-  padding-top: 200px;
-  width: 400px;
-  margin: 0 auto;
+  /* padding-top: 1000px; */
+  width: 450px;
+  margin: 300px 400px;
+}
+</style>
+<style scoped>
+.change-label-class :deep() .el-form-item__label{
+    color: black;
+    font-weight: bold;
 }
 </style>
